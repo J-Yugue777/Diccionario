1757791ffbf8ec314e9ed85a04c0e0c4a810ffd1
jYUGUE <juanjuanyu97@gmail.com>
2025-07-09 20:39:29 -0500

    ejercicio de condicionales

---
+++ condicional.py
@@ -0,0 +1,75 @@
+
+#----------EJERCICIO 1 
+# A=int(input("ingres eun  numero : "))
+# if A > 0:
+#     print(f" el numero {A} es positivo ")
+# elif A < 0:
+#     print(f" {A} es un numero negativo ")
+# else:
+#     print(f" este numero es 0 ")
+#EJERCICIO 2 
+# a =int(input("INGRESE EL PRIMER NUMERO : "))
+# b =int(input("INGRESE EL PRIMER NUMERO : "))
+# if a > b:
+#     print(f"entonces {a} es mayor que {b}")
+# else:
+#     print(f" esntonces {b} es mayor que {a}")
+#-----------EJERCICIO 3
+# E=int(input("ingrese un numero : "))
+# if E % 2 == 0:
+#     print("es par")
+# else:
+#     print("impar")
+#EJERCICIO 4
+# I=int(input("ingrese un numero : "))
+# if I >= 10 and I < 20:
+#     print("esta en el rango")
+# else:
+#     print("error")
+#EJERCICIO 5
+# a=int(input("ingrwese su primer numero : "))
+# e=int(input(" ingrese su segundo numero: "))
+# i=int(input(" ingrese su tercer  numero: "))
+# if a > e and i:
+#     print(f" mayor es {a}segudo de {e} y por ultimo {i}")
+# elif e > i and e:
+#     print(f" mayor es {e}segudo de {i} y por ultimo {e}")
+# elif i > e or a:
+#     print(f" mayor es {i}segudo de {e} y por ultimo {a}")
+#EJERCICIO 6
+# E=int(input("ingrese un precio : "))
+# if E > 100:
+#     q=E*0.1
+#     w=E-q
+#     print(f"su producto esta en {w}")
+# else:
+#     print(f"sigue con el mismo valor {E}")
+#EJERCICIO 7
+# eda=int(input("ingrese su edad : "))
+# if eda <=18:
+#     print(F"usted no puede votar por su edad {eda}")
+# else: 
+#     print("usted si puede votar ")
+#EJERCICIO 8
+# S=input("¿usted es VIP?(SOLO RESPONDA SI O NO) : ")
+# i=int(input("ingrese el valor de su producto : "))
+# if S == "SI":
+#     e=i-(i*0.2)
+#     print(f"el valor de su producto es de {e}")
+# else:
+#     print(f"su valor de {i}")
+#EJERCICIO 9
+# n=int(input("ingrese un numero : "))
+
+# if n %  5 == 0 and n % 3 == 0:
+#     print(f"son multiplos de 3 y 5")
+# else:
+#     print("no son multiplos de 3 y 5 ")
+#EJERCICIO 10
+# e=int(input("ingrese el numero a dividir : "))
+# e1=int(input("ingrese los numeros a dividir : "))
+# e2=int(input("ingrese los numeros a dividir : "))
+# if e %  e2 == 0 and e % e1 == 0:
+#     print(f"es divisible y su resultado da {e/e1} y {e/e2}")
+# else:
+#     print(f"no son divisible ")
