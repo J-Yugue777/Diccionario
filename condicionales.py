769f0aed7f60cf667c0ae0811ef66164b00f270f
jYUGUE <juanjuanyu97@gmail.com>
2025-07-28 17:36:18 -0500

    actualizacion

--- condicionales.py
+++ condicionales.py
@@ -197,10 +197,126 @@
 #     print(f"su valor apagar esta en {f}")
 # else :
 #     print(f"valor es {p}")
+#EJERCICIO 19 
+# a=input("ingrese su nombre : ")
+# e=int(input("ingrse sus horas de trabajo : "))
+# q=10000*e
+# if e>= 40:
+#     print(f"{a} su salario es de : {(q*0.2)+q}")
+# else:
+#     print(f"{a} su salario es de {q}")
+#PRUEBA
+# d={
+#     "a":int(input("dar")),
+#     "e":int(input("dar"))
 
+# }
+# f=d["a"]+ d["e"]
+# li=[]
+# li.append(f)
+# t=tuple(li)
+# print(t)
 
-
-
-
-
-
+#29/07/2025
+#EJERCIO 1
+# a=int(input("ingrese un numero : "))
+# if a > 0:
+#     print("es un positivo ")
+# elif a <=-1:
+#     print("es un negativo ")
+# else:
+#     print("es un cero")
+#EJERCICIO 2
+# a=float(input("ingrese primer valor : "))
+# b=float(input("ingrese segundo valor "))
+# if a>b:
+#     print(f"es {a} mayor a {b}")
+# else:
+#     print(f"es {b} mayor que {a}")
+# a=int(input("ingrese un valor : "))
+# if a % 2 == 0:
+#     print("es par ")
+# else : 
+#     print("es un impar")
+#EJERCICIO 4
+# e=int(input("ingrese un valor : "))
+# if e >=10 and e <=20 :
+#     print("esta en el rango de 10-20")
+# else:
+#     print("no esta el rango de edad")
+#EJERCICIO 5
+# i=int(input("ingrese un numero : "))
+# i1=int(input("ingrese un numero : "))
+# i2=int(input("ingrese un numero : "))
+# if i>i1 and i1>i2:
+#     print(f"el mayor es {i}")
+# elif i1>i and i1>i2:
+#     print(f"{i1} es mayor")
+# else:
+#     print(f"es mayor {i2}")
+#EJERCICIO  6
+# produc=int(input("ingrese el valor del producto : "))
+# if produc >= 100:
+#     print(f"{produc-produc*0.1} es su valor total")
+# else:
+#     print(f"su valor sigue siendo de {produc}")
+#EJERCICIO 7
+# eda=int(input("ingrese su edad : "))
+# if eda>=18:
+#     print("usted es mayor de edad, usted puede votar ")
+# else:
+#     print("usted es menor no puede votar ")
+#EJERCICIO 8
+# catal=input("¿usted posee membresia VIP? (si o no) : ")
+# valo=int(input("ingrese sel costode su compra "))
+# if catal == "si":
+#     print(f"su total a pagar es de {valo-valo*0.2}")
+# else :
+#     print(f"su valor de {valo}")
+#EJERCICIO 9
+# num=int(input("ingrese un numero : "))
+# if num % 5==0 and num % 3 == 0:
+#     print("este numero es multiplo de 5 y 3")
+# else:
+#     print("no es multiplo de 5 y 3 ")
+#EJERCICIO 10 
+# a=int(input("ingrese un numero : "))
+# e=int(input("ingrese un numero divisor : "))
+# i=int(input("ingrse segundo numero divisor : "))
+# if a % e ==0 and a % i == 0 :
+#     print(f"es divisible por {e} da {a/e} y {i} da {a/i}")
+# else:
+#     print("no es divisible por los dos ")
+#EJERCICIO 11
+# lis=[int(input("ingrse numero : ")),int(input("ingrse numero : ")),int(input("ingrse numero : ")),int(input("ingrse numero : ")),int(input("ingrse numero : "))]
+# if lis[2] >= 10:
+#     print(" mayor a 10 ")
+# else:
+#     print("menor a 10 ")
+#EJERCICIO 12
+# li=[int(input("ingrese un numero :")),int(input("ingrese un numero :")),int(input("ingrese un numero :")),int(input("ingrese un numero :")),int(input("ingrese un numero :"))]
+# if li[0] == 7 or li[1]==7 or li[2]==7 or li[3]==7 or li[4]==7:
+#     print("si esta ")
+# else:
+#     print("no esta ")
+# print(li)
+#EJERCICIO 13
+# li=[int(input("ingrese un numero a la lista ")),int(input("ingrese un numero a la lista ")),int(input("ingrese un numero a la lista ")),int(input("ingrese un numero a la lista "))]
+# if li[0]+li[1]>=10:
+#     print("es mayor a 10 ")
+# else :
+#     print("es menor a 10 ")
+#EJERCICIO 14
+# lis=["ana","luis","pedro","marta"]
+# if lis[0]=="marta" or lis[1]=="marta" or lis[2]=="marta" or lis[3]=="marta":
+#     print("si esta en la lista ")
+# else:
+#     print("no esta en la lista ")
+# print(lis)
+#EJERCICIO 15
+l=[input("ingrse un color"),input("ingrse un color"),input("ingrse un color")]
+if l[1]=="azul":
+    nclor=input("ingrese nuevo color : ")
+    print(l.insert)
+else:
+    print(l)
